c6aafe4794035b7eca07ea5eb5d8a484abe234cd
Sachin Chavan <sachin.chavan043@gmail.com>
2020-03-14 00:46:37 +0530

    New program added

--- Scala_DataFrames/src/main/scala/com/tgt/spark/SQL/ProgramSQL7.scala
+++ Scala_DataFrames/src/main/scala/com/tgt/spark/SQL/ProgramSQL7.scala
@@ -5,12 +5,10 @@ import org.apache.spark.SparkContext
 import org.apache.spark.sql._
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.SQLImplicits
+import org.apache.spark.sql.execution.datasources.csv.CSVFileFormat
 
 object ProgramSQL7 {
   
-  case class Employee(empno:String, ename:String, designation:String, manager:String, 
-                      hire_date:String, sal:String, deptno:String)
- 
   def main(args: Array[String]){
     
   val spark = SparkSession
@@ -23,15 +21,20 @@ object ProgramSQL7 {
    val sc = spark.sparkContext
    import spark.implicits._
   
-   val textRDD = sc.textFile("C:/Users/CSC/git/SparkSQL/Scala_DataFrames/Files/emp_data.csv")
-  
-   val empRDD = textRDD.map
-   {  line =>  val col = line.split(",")
-      Employee(col(0), col(1), col(2), col(3), col(4), col(5), col(6))    
-   }
-  
-   val empDF = empRDD.toDF()
-   empDF.show()
+   // Create a DataFrame from reading a CSV file
+   val dfFile = spark
+               .read
+               .format("com.databricks.spark.csv")
+               .option("inferSchema", "true")
+               .option("header","true")
+               .load("C:/Users/CSC/git/SparkSQL/Scala_DataFrames/Files/question_tags_10K.csv")
+               .toDF("id", "tag")
+               
+   //val dfs = sqlcontext.read.json("C:/Users/CSC/workspace/Scala_DataFrames/Files/employee.json")
+               
+   dfFile.show(10)
+   
+   
   }
   
 }
